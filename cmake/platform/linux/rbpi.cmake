5a949f6fe7af673a9cfdc0a0af2a8bdfc515051b
Adrian Cuzman <adriancuzman@gmail.com>
2018-09-12 15:51:51 +0300

    added /opt/vc/include for rbpi

--- cmake/platform/linux/rbpi.cmake
+++ cmake/platform/linux/rbpi.cmake
@@ -1,3 +1,4 @@
 set(PLATFORM_REQUIRED_DEPS OpenGLES EGL LibInput Xkbcommon)
 set(APP_RENDER_SYSTEM gles)
 list(APPEND PLATFORM_DEFINES -D_ARMEL -DTARGET_RASPBERRY_PI)
+include_directories(${INCLUDES} "/opt/vc/include")
